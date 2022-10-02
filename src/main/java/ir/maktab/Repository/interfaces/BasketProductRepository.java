91a9f2fb9fe4c30492a85dad394257ce1b6a81e0
somaye6868 <somayekariminia6868@gmail.com>
2022-10-02 13:55:54 +0330

    add interface Store

--- src/main/java/ir/maktab/Repository/interfaces/BasketProductRepository.java
+++ src/main/java/ir/maktab/Repository/interfaces/BasketProductRepository.java
@@ -1,2 +1,25 @@
-package ir.maktab.Repository.interfaces;public class BasketProductRepository {
+package ir.maktab.Repository.interfaces;
+
+import java.util.List;
+
+public class BasketProductRepository<T> implements BasketProduct<T> {
+    @Override
+    public void addProductToBasket(Object product) {
+
+    }
+
+    @Override
+    public void deleteProductOfBasket(Object product) {
+
+    }
+
+    @Override
+    public List printProducts() {
+        return null;
+    }
+
+    @Override
+    public double printTotalPrice() {
+        return 0;
+    }
 }
