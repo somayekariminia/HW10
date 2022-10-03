49aae914690356aa6dde36a9da4ca1061cb9b831
somaye6868 <somayekariminia6868@gmail.com>
2022-10-03 10:43:56 +0330

    add connection.java class

--- src/main/java/ir/maktab/model/entity/Device.java
+++ src/main/java/ir/maktab/model/entity/Device.java
@@ -1,7 +1,2 @@
-package ir.maktab.model.entity;
-
-import ir.maktab.model.enums.DevicesType;
-
-public class Device extends Item {
-    private DevicesType devicesType;
+package ir.maktab.model.entity;public class Device {
 }
--- src/main/java/ir/maktab/model/entity/Shoes.java
+++ src/main/java/ir/maktab/model/entity/Shoes.java
@@ -1,7 +1,2 @@
-package ir.maktab.model.entity;
-
-import ir.maktab.model.enums.ShoesType;
-
-public class Shoes extends Item {
-    private ShoesType shoesType;
+package ir.maktab.model.entity;public class Shoes {
 }
