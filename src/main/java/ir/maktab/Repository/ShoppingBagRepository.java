ed92cf03e4eb297007b764c3a6316409ec407eb8
somaye6868 <somayekariminia6868@gmail.com>
2022-10-03 20:42:51 +0330

    delete ShoppingBag interfaces package repository

--- src/main/java/ir/maktab/Repository/ShoppingBagRepository.java
+++ src/main/java/ir/maktab/Repository/ShoppingBagRepository.java
@@ -1,21 +1,17 @@
 package ir.maktab.Repository;
 
 import ir.maktab.Repository.cconnection.GetConnection;
-import ir.maktab.Repository.interfaces.ShoppingBag;
-import ir.maktab.model.entity.Item;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-public class ShoppingBagRepository<T extends Item> implements ShoppingBag<T> {
+public class ShoppingBagRepository {
 //    private Map<Item,Integer> mapShopping=new HashMap<>();
 //    private boolean isConfirm;
 //    private int capacity;
-    @Override
+
     public void addItemToShoppingBag(int userId,int itemId,int count,String type) throws SQLException {
         Connection connection= GetConnection.getConnection();
         PreparedStatement preparedStatement=connection.prepareStatement("insert into shoppingbag(UserId,ItemId,count,typer) values (?,?,?,?)");
@@ -26,7 +22,7 @@ public class ShoppingBagRepository<T extends Item> implements ShoppingBag<T> {
         preparedStatement.executeUpdate();
     }
 
-    @Override
+
     public void deleteItemOfShoppingBag(int userId,int itemId ) throws SQLException {
     Connection connection=GetConnection.getConnection();
     PreparedStatement preparedStatement=connection.prepareStatement("delete from shoppingbage where idItem=? and idItem=?");
@@ -35,7 +31,7 @@ public class ShoppingBagRepository<T extends Item> implements ShoppingBag<T> {
     preparedStatement.executeUpdate();
     }
 
-    @Override
+
     public List printAllItems(int id) throws SQLException {
         Connection connection=GetConnection.getConnection();
         PreparedStatement preparedStatement=connection.prepareStatement("select * from shoppingbag inner join item on shoppingbag.itemId=item.id where userId=?");
