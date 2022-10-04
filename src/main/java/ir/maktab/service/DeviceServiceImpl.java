162ad16dcc71899a972019783fef8905df97cea9
somaye6868 <somayekariminia6868@gmail.com>
2022-10-04 09:45:51 +0330

    add ReadingServiceImpl class

---
+++ src/main/java/ir/maktab/service/DeviceServiceImpl.java
@@ -0,0 +1,16 @@
+package ir.maktab.service;
+
+import ir.maktab.model.entity.Device;
+
+public class ItemServiceDeviceImpl implements ItemService<Device> {
+    @Override
+    public void updateStockItems(Device item, int count) {
+
+    }
+
+    @Override
+    public int getIdItem(Device item) {
+        return 0;
+    }
+
+}
