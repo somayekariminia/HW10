6b900d7d05f94a997f5c69a7623ae50c9fdf69d9
somaye6868 <somayekariminia6868@gmail.com>
2022-10-08 18:35:07 +0330

    add method validationInputToMenue

--- src/main/java/ir/maktab/util/ValidationNameUser.java
+++ src/main/java/ir/maktab/util/ValidationNameUser.java
@@ -15,7 +15,18 @@ public class ValidationNameUser {
             return password;
         else
             throw new ValidationExeption("password is not valid");
-
+    }
+    public static  int validationInputToMenu(String input) throws ValidationExeption {
+    if(input.matches("[1-9]"))
+        return Integer.parseInt(input);
+    else
+        throw  new ValidationExeption("please enter a number");
+    }
+    public static  String validationCodeProduct(String input) throws ValidationExeption {
+        if(input.matches("[1-9]{3}[a-z]*"))
+            return input;
+        else
+            throw new ValidationExeption("codeProduct is not valid");
 
     }
 
